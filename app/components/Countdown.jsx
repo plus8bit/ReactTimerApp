9c2e9a741b3ee61ec756d5ee699fc5499604e0bf
Nick <plus0bit@gmail.com>
2017-04-29 23:02:15 +0300

    Create clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,8 +1,13 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
-  render: function() {
-    return <p>Countdown.jsx</p>;
+  render: function () {
+    return (
+      <div>
+        <Clock totalSeconds={129}/>
+      </div>
+    );
   }
 });
 
